0d44ce5a137054aea439a9ccc323d1abd22a8c60
renechoi <renechoi90@gmail.com>
2023-03-29 12:01:57 +0900

    feat: 뷰 컨트롤러 엔드포인트 실제 구현 코드 작성

--- board-admin/src/main/java/fast/boardadmin/controller/MainController.java
+++ board-admin/src/main/java/fast/boardadmin/controller/MainController.java
@@ -1,9 +1,16 @@
 package fast.boardadmin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("")
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+
+    public String root(){
+        return "forward:/management/articles";
+    }
 }
