58576eadbc37892c4a68f0b864eb321de5c905fb
renechoi <renechoi90@gmail.com>
2023-03-30 01:06:47 +0900

    feat: 게시판 서비스 호스트명 프로퍼티 등록

---
+++ board-admin/src/main/java/fast/boardadmin/dto/properties/ProjectProperties.java
@@ -0,0 +1,10 @@
+package fast.boardadmin.dto.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties("project")
+public record ProjectProperties(Board board) {
+
+    public record Board(String url){}
+}
