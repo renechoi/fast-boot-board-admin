297ecfe5a0d00178a1a54f3d6073a5871dc53f23
renechoi <renechoi90@gmail.com>
2023-03-29 11:48:55 +0900

    feat: 뷰 컨트롤러 엔드포인트 테스트 코드 작성

---
+++ board-admin/src/main/java/fast/boardadmin/controller/UserAccountManagementController.java
@@ -0,0 +1,9 @@
+package fast.boardadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/management/user-accounts")
+@Controller
+public class UserAccountManagementController {
+}
