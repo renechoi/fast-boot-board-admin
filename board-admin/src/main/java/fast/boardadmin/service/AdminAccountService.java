e899e38b3c854da12c57ea4591d7cbc0ad7eec8a
renechoi <renechoi90@gmail.com>
2023-03-30 17:25:15 +0900

    feat: 회원 서비스 컨트롤러 기능 구현

--- board-admin/src/main/java/fast/boardadmin/service/AdminAccountService.java
+++ board-admin/src/main/java/fast/boardadmin/service/AdminAccountService.java
@@ -1,35 +1,45 @@
 package fast.boardadmin.service;
 
+import fast.boardadmin.domain.AdminAccount;
 import fast.boardadmin.domain.constant.RoleType;
 import fast.boardadmin.dto.AdminAccountDto;
 import fast.boardadmin.repository.AdminAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class AdminAccountService {
 
     private final AdminAccountRepository adminAccountRepository;
 
+    @Transactional(readOnly = true)
     public Optional<AdminAccountDto> searchUser(String username) {
-        return Optional.empty();
+        return adminAccountRepository.findById(username)
+                .map(AdminAccountDto::from);
     }
 
     public AdminAccountDto saveUser(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
-        return null;
+        return AdminAccountDto.from(
+                adminAccountRepository.save(AdminAccount.of(username, password, roleTypes, email, nickname, memo))
+        );
     }
 
+    @Transactional(readOnly = true)
     public List<AdminAccountDto> users() {
-        return List.of();
+        return adminAccountRepository.findAll().stream()
+                .map(AdminAccountDto::from)
+                .toList();
     }
 
     public void deleteUser(String username) {
-
+        adminAccountRepository.deleteById(username);
     }
 
 }
