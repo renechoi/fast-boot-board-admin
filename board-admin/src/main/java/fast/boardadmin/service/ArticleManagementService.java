3976c4f51ce9b29b25944eb04b013199b3f087e6
renechoi <renechoi90@gmail.com>
2023-03-30 13:09:36 +0900

    feat: general bug fix for spring data rest test

--- board-admin/src/main/java/fast/boardadmin/service/ArticleManagementService.java
+++ board-admin/src/main/java/fast/boardadmin/service/ArticleManagementService.java
@@ -1,20 +1,37 @@
 package fast.boardadmin.service;
 
 import fast.boardadmin.dto.ArticleDto;
+import fast.boardadmin.dto.properties.ProjectProperties;
+import fast.boardadmin.dto.response.ArticleClientResponse;
+import fast.boardadmin.dto.response.ArticleCommentClientResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleManagementService {
 
+    private final RestTemplate restTemplate;
+    private final ProjectProperties projectProperties;
+
     public List<ArticleDto> getArticles(){
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/articles")
+                .queryParam("size", 10000)
+                .build()
+                .toUri();
+
+        ArticleClientResponse articleClientResponse = restTemplate.getForObject(uri, ArticleClientResponse.class);
+
+
         return List.of();
     }
 
