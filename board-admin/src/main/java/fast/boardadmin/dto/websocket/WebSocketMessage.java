fe22e7dc5ac000b85c775cc0bb7e87a5fd949c43
renechoi <renechoi90@gmail.com>
2023-03-30 21:58:17 +0900

    feat: 어드민 페이지 채팅 기능 구현

---
+++ board-admin/src/main/java/fast/boardadmin/dto/websocket/WebSocketMessage.java
@@ -0,0 +1,8 @@
+package fast.boardadmin.dto.websocket;
+
+public record WebSocketMessage(String content) {
+
+    public static WebSocketMessage of(String content){
+        return new WebSocketMessage(content);
+    }
+}
